9de594fafb3a230842f755b308d9a0a83547d5d8
Bahati Godfrey <godfreygabanell4@gmail.com>
2022-09-30 09:13:41 +0300

    update

--- login.php
+++ login.php
@@ -14,13 +14,13 @@ if (isset($_POST['login'])) {
 	$email = $_POST['email'];
 	$password = md5($_POST['password']);
 
-	$sql = "SELECT * FROM registeredusers WHERE email='$email' AND userpassword='$password'";
+	$sql = "SELECT * FROM signup WHERE email='$email' AND password='$password'";
 	$result = mysqli_query($conn, $sql);
 	if ($result->num_rows > 0) {
 		$row = mysqli_fetch_assoc($result);
         echo 'Login success';
 		//$_SESSION['username'] = $row['username'];
-		header("Location: dashboard.php");
+		header("Location: index.php");
 	} else {
 		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
 	}
